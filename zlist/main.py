b30fcefa317c4306fca44005bba31e2e89ca3d64
James Tiplady <biggleszx@biggleszx.com>
2014-03-21 11:20:21 +0000

    Converting to package and spring-cleaning

--- zlist/main.py
+++ zlist/main.py
@@ -1,16 +1,42 @@
 '''
-Parse a CSS file for elements with a defined z-index and list them
-
-Usage: $ python zlist.py <file.css> <file2.css> ..
+This is the primary touchpaper module; the `main` function is called when you
+run `zlist` on the command line.
 '''
 
+import argparse
 import cssutils
 import logging
 import sys
+
+from ._version import get_version
+
+
 cssutils.log.setLevel(logging.CRITICAL)
 
+
+'''
+Main package routine
+
+Parse CSS files for elements with a defined z-index and list them
+
+Usage: $ python zlist.py <file.css> <file2.css> ..
+'''
 def main():
-    for filename in sys.argv[1:]:
+    ''' Argument parser init '''
+    parser = argparse.ArgumentParser(description='Parse CSS files for elements '
+                                                 'with a defined z-index and '
+                                                 'list them')
+    parser.add_argument('-v', '--version', dest='version', action='store_true',
+                        help='show package version information and exit')
+    parser.add_argument('files', nargs='*')
+    args = parser.parse_args()
+
+    if args.version:
+        print "zlist v%s" % get_version()
+        sys.exit(0)
+
+    ''' Iterate files supplied as args and parse them '''
+    for filename in args.files:
         sheet = cssutils.parseFile(filename)
         zlist = []
         for rule in sheet:
@@ -34,7 +60,7 @@ def main():
             print ""
         else:
             print "%s: No z-index declarations found" % filename
-        
+
 
 if __name__ == "__main__":
     main()
